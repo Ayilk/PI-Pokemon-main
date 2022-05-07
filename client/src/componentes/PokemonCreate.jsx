5af861b849f2234fdb69c3121dabc107c73fc6dc
Ayilk <srangel.h92@gmail.com>
2022-05-06 23:31:58 -0500

    avance formulario

--- client/src/componentes/PokemonCreate.jsx
+++ client/src/componentes/PokemonCreate.jsx
@@ -11,16 +11,60 @@ export default function PokemonCreate(){
         name: "", imagen: "", types: [], id:"", hp:"", speed:"",
         attack:"", defense: "",  height: "", weight:""
     })
+    const [errors, setErrors] = useState({});
+
+    const validate = (input) =>{
+        let errors = {};
+    
+      if (!input.name) {
+        errors.name = "A name is required";
+      } else if (!/^[a-zA-Z]+$/.test(input.name) || input.name.length > 10) {
+        errors.name = "Name is invalid";
+      }
+    
+      if (!input.height) {
+        errors.height = "height is required";
+      } else if (input.height > 200) {
+        errors.height = "height is invalid";
+      }
+    
+      if (!input.weight) {
+        errors.weight = "weight is required";
+      } else if (input.weight > 2000) {
+        errors.weight = "weight is invalid";
+      }
+    
+      if (!input.img) {
+        errors.img = "an img url is required";
+      } else if (!/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/.test(input.img)
+    ) {errors.img = "url is invalid";}
+    
+      return errors;
+    }
 
     useEffect(()=> {
         dispatch(getTypes())
     },[dispatch])
 
     function handleChange(e){
-        setInput({
-            ...input,
-            [ e.target.name ] : e.target.value
-        })
+         if (e.target.name === "name" || e.target.name === "img") {
+      setInput({
+        ...input,
+        [e.target.name]: e.target.value,
+      });
+    } else {
+      setInput({
+        ...input,
+        [e.target.name]: Number(e.target.value),
+      });
+    }
+
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
     }
 
     function handleSelect(e){
